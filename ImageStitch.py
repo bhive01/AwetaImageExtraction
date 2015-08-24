ead51918a031bbf3d76329418a7425ae1ba67054
Brandon Hurr <brandon.hurr@gmail.com>
2015-08-24 14:07:50 -0700

    Update ImageStitch.py

--- ImageStitch.py
+++ ImageStitch.py
@@ -21,9 +21,14 @@ for val in filecount:
 
 looper = max(filedigits)
 
+currentdir = os.getcwd()
+
+os.chdir(directory_name)
+
 for x in range(1,looper):
-  montagename = 'find %s -name \"file%03d*.png\" -print0 | cat - <(echo -ne \"montage%03d.png\") | xargs -0 montage -tile x4 -geometry +0+0' % (directory_name, x, x)
+  montagename = 'find . -name \"file%03d*.png\" -print0 | cat - <(echo -ne \"montage%03d.png\") | xargs -0 montage -tile x4 -geometry +0+0' % (x, x)
   #send assembled system command
   os.system(montagename)
   
+os.chdir(currentdir)
 
