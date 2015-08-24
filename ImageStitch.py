9585b04a3b6695036e2736c6a695ff0552b48d3e
Brandon Hurr <brandon.hurr@gmail.com>
2015-08-24 14:42:09 -0700

    Update ImageStitch.py

--- ImageStitch.py
+++ ImageStitch.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import time
+import subprocess
 
 try:
     directory_name=sys.argv[1]
@@ -29,7 +30,7 @@ for x in range(1,looper+1):
   montagename = "find . -name \"file%03d*.png\" -print0 | cat - <\(echo -ne \"montage%03d.png\"\) | xargs -0 montage -tile x4 -geometry +0+0" % (x, x)
   print(montagename)
   #send assembled system command
-  os.system(montagename)
+  subprocess.call(['bash', '-c', montagename])
   
 os.chdir(currentdir)
 
