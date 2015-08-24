85ac72a6e00c8ce3f7a7a9316455888992576956
Brandon Hurr <brandon.hurr@gmail.com>
2015-08-24 14:18:49 -0700

    Update ImageStitch.py
    loops in python start at 0 you nub

--- ImageStitch.py
+++ ImageStitch.py
@@ -25,7 +25,7 @@ currentdir = os.getcwd()
 
 os.chdir(directory_name)
 
-for x in range(1,looper):
+for x in range(0,looper):
   montagename = "find . -name \"file%03d*.png\" -print0 | cat - <(echo -ne \"montage%03d.png\") | xargs -0 montage -tile x4 -geometry +0+0" % (x, x)
   #send assembled system command
   os.system(montagename)
