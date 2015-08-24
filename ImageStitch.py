7e28474c73b724354c0ff4b14b766fd87e31296c
Brandon Hurr <brandon.hurr@gmail.com>
2015-08-24 14:32:13 -0700

    Update ImageStitch.py

--- ImageStitch.py
+++ ImageStitch.py
@@ -27,6 +27,7 @@ os.chdir(directory_name)
 
 for x in range(1,looper+1):
   montagename = "find . -name \"file%03d*.png\" -print0 | cat - <\(echo -ne \"montage%03d.png\"\) | xargs -0 montage -tile x4 -geometry +0+0" % (x, x)
+  print(montagename)
   #send assembled system command
   os.system(montagename)
   
